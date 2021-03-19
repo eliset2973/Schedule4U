609488b4b693ccf6a9556fdf15829eda6ea6f5fe
Elise Tran <tran0592@umn.edu>
2021-03-19 13:22:52 -0500

    Adding Productivity About me

--- Schedule4U/app/src/main/java/com/example/schedule4u/ProductivityAboutMe.java
+++ Schedule4U/app/src/main/java/com/example/schedule4u/ProductivityAboutMe.java
@@ -1,23 +1,21 @@
 package com.example.schedule4u;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-import android.widget.Button;
-
-public class Productivity extends AppCompatActivity {
-
+public class ProductivityAboutMe extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_productivity);
+        setContentView(R.layout.activity_productivity_about_me);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -30,13 +28,6 @@ public class Productivity extends AppCompatActivity {
             }
         });
 
-        Button todo = (Button) findViewById(R.id.button7);
-        todo.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), ProductivityAboutMe.class);
-                startActivityForResult(myIntent, 0);
-            }
 
-        });
     }
 }
