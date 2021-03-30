58c5dae19b0bdcf4a580d96d384d3c1d700f5eae
Ioana Munteanu <munte029@umn.edu>
2021-03-30 02:19:19 -0500

    progress bar fixed

--- Schedule4U/app/src/main/java/com/example/schedule4u/Productivity.java
+++ Schedule4U/app/src/main/java/com/example/schedule4u/Productivity.java
@@ -12,6 +12,9 @@ import android.graphics.Matrix;
 import java.io.ByteArrayOutputStream;
 import java.util.List;
 
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -46,13 +49,9 @@ public class Productivity extends AppCompatActivity {
 
         int total_finished = 0;
         int total_not_finished = 0;
-        int total_leisure = 0;
         Activity_S4U_Data_Accessor accessor = new Activity_S4U_Data_Accessor(getApplicationContext(),true);
         List<Activity_S4U> displayList = accessor.lists.active;
         for (Activity_S4U myActivity : displayList){
-            if (myActivity.importance_string == "Leisure"){
-                total_leisure += myActivity.time_alotted;
-            }
             if (myActivity.completed){
                 total_finished += myActivity.time_alotted;
             }
@@ -60,9 +59,54 @@ public class Productivity extends AppCompatActivity {
                 total_not_finished += myActivity.time_alotted;
             }
         }
-        System.out.println("total_leisure: " + total_leisure);
+        int total = total_not_finished+total_finished;
         System.out.println("total_finished: " + total_finished);
         System.out.println("total_not_finished: " + total_not_finished);
+        String finished = "Finished tasks: "+total_finished/60 + "hr " + total_finished%60 + "min";
+        String not_finished = "Tasks still in progress: "+total_not_finished/60 + "hr " + total_not_finished%60 + "min";
+        TextView old_finished = (TextView) findViewById(R.id.prod_text2);
+        old_finished.setText(finished);
+        TextView old_not_finished = (TextView) findViewById(R.id.prod_text1);
+        old_not_finished.setText(not_finished);
+        ProgressBar progressBar1= (ProgressBar) findViewById(R.id.progressBar1);
+        progressBar1.setProgress(total_not_finished*100/total);
+        ProgressBar progressBar2= (ProgressBar) findViewById(R.id.progressBar2);
+        progressBar2.setProgress(total_finished*100/total);
+
+    }
+
+    protected void onResume(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productivity);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        int total_finished = 0;
+        int total_not_finished = 0;
+        Activity_S4U_Data_Accessor accessor = new Activity_S4U_Data_Accessor(getApplicationContext(),true);
+        List<Activity_S4U> displayList = accessor.lists.active;
+        for (Activity_S4U myActivity : displayList){
+            if (myActivity.completed){
+                total_finished += myActivity.time_alotted;
+            }
+            else {
+                total_not_finished += myActivity.time_alotted;
+            }
+        }
+        int total = total_not_finished+total_finished;
+        System.out.println("total_finished: " + total_finished);
+        System.out.println("total_not_finished: " + total_not_finished);
+        String finished = "Finished tasks: "+total_finished/60 + "hr " + total_finished%60 + "min";
+        String not_finished = "Tasks still in progress: "+total_not_finished/60 + "hr " + total_not_finished%60 + "min";
+        TextView old_finished = (TextView) findViewById(R.id.prod_text2);
+        old_finished.setText(finished);
+        TextView old_not_finished = (TextView) findViewById(R.id.prod_text1);
+        old_not_finished.setText(not_finished);
+        ProgressBar progressBar1= (ProgressBar) findViewById(R.id.progressBar1);
+        progressBar1.setProgress(total_not_finished*100/total);
+        ProgressBar progressBar2= (ProgressBar) findViewById(R.id.progressBar2);
+        progressBar2.setProgress(total_finished*100/total);
 
     }
 }
