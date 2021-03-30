60b7427517fccc3dbd7d7fbd77bd4bc5ba14d198
harty409 <80783509+harty409@users.noreply.github.com>
2021-03-29 22:00:28 -0700

    failed attempt to check date

--- Schedule4U/app/src/main/java/com/example/schedule4u/Calendar.java
+++ Schedule4U/app/src/main/java/com/example/schedule4u/Calendar.java
@@ -12,42 +12,50 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Calendar;
 
 public class Calendar extends AppCompatActivity {
+    long today_date;
+    protected void displayListBelowCalendar(List<Activity_S4U> displayList) {
+        TextView textViewCalendarActivityName1 = (TextView) findViewById(R.id.textViewCalendarActivityName1);
+        TextView textViewCalendarActivityName2 = (TextView) findViewById(R.id.textViewCalendarActivityName2);
+        TextView textViewCalendarActivityName3 = (TextView) findViewById(R.id.textViewCalendarActivityName3);
+        if (displayList.size()>0) {
+            textViewCalendarActivityName1.setText(displayList.get(0).name);
+            //System.out.println(displayList.get(0).name);
+        } else {
+            textViewCalendarActivityName1.setText("");
+        }
+
+        try {
+            if (displayList.size()>1) {
+                textViewCalendarActivityName2.setText(displayList.get(1).name);
+            } else {
+                textViewCalendarActivityName2.setText("");
+            }
+            if (displayList.size()>2) {
+                textViewCalendarActivityName3.setText(displayList.get(2).name);
+            } else {
+                textViewCalendarActivityName3.setText("");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 @Override
 protected void onResume() {
     super.onResume();
     // change to content layout
     Activity_S4U_Data_Accessor accessor = new Activity_S4U_Data_Accessor(getApplicationContext(),true);
     List<Activity_S4U> displayList = accessor.lists.active;
-    TextView textViewCalendarActivityName1 = (TextView) findViewById(R.id.textViewCalendarActivityName1);
-    TextView textViewCalendarActivityName2 = (TextView) findViewById(R.id.textViewCalendarActivityName2);
-    TextView textViewCalendarActivityName3 = (TextView) findViewById(R.id.textViewCalendarActivityName3);
-    if (displayList.size()>0) {
-        textViewCalendarActivityName1.setText(displayList.get(0).name);
-        //System.out.println(displayList.get(0).name);
-    } else {
-        textViewCalendarActivityName1.setText("");
-    }
-
-    try {
-        if (displayList.size()>1) {
-            textViewCalendarActivityName2.setText(displayList.get(1).name);
-        } else {
-            textViewCalendarActivityName2.setText("");
-        }
-        if (displayList.size()>2) {
-            textViewCalendarActivityName3.setText(displayList.get(2).name);
-        } else {
-            textViewCalendarActivityName3.setText("");
-        }
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
+    displayListBelowCalendar(displayList);
 }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,6 +113,29 @@ protected void onResume() {
             }
         });
         //Be careful! This is code for image button not ordinarily button//
+
+
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener (new CalendarView.OnDateChangeListener() {
+           public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+               CalendarView calendarView2 = (CalendarView)findViewById(R.id.calendarView);
+               long current_selected_date = calendarView2.getDate();
+               today_date = System.currentTimeMillis();
+               Date date1 = (new Date(current_selected_date));
+               Date date2 = (new Date(today_date));
+               Calendar calendar2 = Calendar.getInstance();
+               boolean sameDate = (month==date2.getMonth())&&(dayOfMonth==date2.getDay());
+               System.out.println(date1.toString() + " compared to " + date2.toString() + " and " +year+" "+month+" "+dayOfMonth+" and "+date2.getYear()+" "+date2.getMonth()+" "+date2.getDay());
+                if((sameDate)) {
+                   Activity_S4U_Data_Accessor accessor = new Activity_S4U_Data_Accessor(getApplicationContext(),true);
+                   List<Activity_S4U> displayList = accessor.lists.active;
+                   displayListBelowCalendar(displayList);
+               } else {
+                    displayListBelowCalendar(new ArrayList<Activity_S4U>());
+                }
+
+           }
+        });
 
 
     }
